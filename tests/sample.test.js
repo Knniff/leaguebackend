d3943a820293f2e6fa8cb1c14308ab8310947b02
Knniff <Knniff@gmx.de>
2020-01-23 21:22:08 +0100

    add test library

---
+++ tests/sample.test.js
@@ -0,0 +1,21 @@
+describe('Sample Test', () => {
+    it('should test that true === true', () => {
+      expect(true).toBe(true)
+    })
+  })
+/* 
+const request = require('supertest')
+const app = require('../server')
+describe('Post Endpoints', () => {
+  it('should create a new post', async () => {
+    const res = await request(app)
+      .post('/api/posts')
+      .send({
+        userId: 1,
+        title: 'test is cool',
+      })
+    expect(res.statusCode).toEqual(201)
+    expect(res.body).toHaveProperty('post')
+  })
+})
+*/
