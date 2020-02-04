03f3de28b0ceb08a5095537d855badf6f870a98f
Knniff <Knniff@gmx.de>
2020-02-04 16:24:24 +0100

    handle jwt-express errors

--- _helpers/error-handler.js
+++ _helpers/error-handler.js
@@ -2,6 +2,16 @@ const log = require("./logger");
 // eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
   // default error
+  if (err.name === "UnauthorizedError") {
+    err.statusCode = 422;
+    err.name = "Unauthorized.";
+    if (err.message === "jwt malformed") {
+      err.message = "JWT malformed.";
+    }
+  }
+  if (err.statusCode === undefined) {
+    err.statusCode = 500;
+  }
   log.logger.warn(err.message);
   return res
     .status(err.statusCode)
